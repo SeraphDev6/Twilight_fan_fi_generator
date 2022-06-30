8211f49410c6257cec0ba485bb922ca1f7fefc47
Wesley <wesley.giles@gmail.com>
2022-06-30 16:06:43 -0400

    small fixes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import tensorflow as tf
-import numpy as np
+from tensorflow import saved_model, strings, constant
 app = FastAPI()
 
 
@@ -12,15 +11,16 @@ app.add_middleware(
   allow_headers=["*"]
 )
 
-model = tf.saved_model.load("./twilightify")
+model = saved_model.load("./twilightify")
 
 @app.get("/")
 def get_twilight(q:str = "Twilight:",chars=""):
-  next_char = tf.constant([q])
+  states = None
+  next_char = constant([q])
   result = [next_char]
 
   for n in range(1000):
     next_char, states = model.generate_one_step(next_char, states=states)
     result.append(next_char)
-  result = tf.strings.join(result)
+  result = strings.join(result)
   return {"result": result[0].numpy().decode('utf-8')}
