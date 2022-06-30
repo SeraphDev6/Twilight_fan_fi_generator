fc86d3f11e3984cfd9bca06170993a70b96f1fd0
Wesley <wesley.giles@gmail.com>
2022-06-30 16:31:03 -0400

    fixed char length param

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ app.add_middleware(
 model = saved_model.load("./twilightify")
 
 @app.get("/")
-def get_twilight(q:str = "Twilight:",chars=""):
+def get_twilight(q:str = "Twilight:",chars:int=1000):
   states = None
   next_char = constant([q])
   result = [next_char]
 
-  for n in range(1000):
+  for n in range(chars):
     next_char, states = model.generate_one_step(next_char, states=states)
     result.append(next_char)
   result = strings.join(result)
