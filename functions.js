33d574c3ba19d9e51943f48ba20e64712cb1e9a0
Wesley <wesley.giles@gmail.com>
2022-06-30 18:45:36 -0400

    final fixes

--- functions.js
+++ functions.js
@@ -1,14 +1,16 @@
 const form = document.getElementById("main_form")
 const text = document.getElementById("storyStart")
 const final =  document.getElementById("finalStory")
-
+const submit = document.getElementById("submit")
 form.addEventListener("submit",(e)=>{
   e.preventDefault();
   const start = text.value;
   const size = document.querySelector('input[name="size"]:checked').value;
+  submit.disabled = true;
   fetch(`https://twilightify.herokuapp.com/?q=${encodeURIComponent(start)}&chars=${size}`)
   .then(res => res.json())
   .then(data => data.result)
   .then(data => final.innerText = data)
+  .then(() => setTimeout(()=> submit.disabled = false, 1000))
   .catch(e => console.log(e))
 })
