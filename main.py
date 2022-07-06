5e029b02eedc8d7ea2fb1002132f0f3b9fc4faf4
Wesley <wesley.giles@gmail.com>
2022-07-06 11:59:35 -0400

    added philipify

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ app.add_middleware(
   allow_headers=["*"]
 )
 
-model = saved_model.load("./twilightify")
+twilight_model = saved_model.load("./twilightify")
+philip_model = saved_model.load("./Philipify")
 
 @app.get("/")
 def get_twilight(q:str = "Twilight:",chars:int=1000):
@@ -20,7 +21,19 @@ def get_twilight(q:str = "Twilight:",chars:int=1000):
   result = [next_char]
 
   for n in range(chars):
-    next_char, states = model.generate_one_step(next_char, states=states)
+    next_char, states = twilight_model.generate_one_step(next_char, states=states)
+    result.append(next_char)
+  result = strings.join(result)
+  return {"result": result[0].numpy().decode('utf-8')}
+
+@app.get("/philip")
+def get_philip(q:str = "Twilight:",chars:int=1000):
+  states = None
+  next_char = constant([q])
+  result = [next_char]
+
+  for n in range(chars):
+    next_char, states = philip_model.generate_one_step(next_char, states=states)
     result.append(next_char)
   result = strings.join(result)
   return {"result": result[0].numpy().decode('utf-8')}
